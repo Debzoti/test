f14b0802d0765c34bc62d60bb77413de2d4aab46
debjyoti <debjyotisarkar929@gmail.com>
2026-06-09 14:19:56 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,19 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-const config = defineConfig({
+export default defineConfig({
   resolve: {
     tsconfigPaths: true,
   },
+
   plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+
   server: {
     host: "0.0.0.0",
+    port: 3000,
+    strictPort: true,
     cors: {
       origin: ["https://app2.tt.debzoti.qzz.io"],
     },
   },
 });
-
-export default config;
